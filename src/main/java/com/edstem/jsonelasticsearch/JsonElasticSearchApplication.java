3956d82d3760a32593ae363d931d6fc6f17b0cfd
devanand-jrx <devanand.ravindran@edstem.com>
2024-04-05 11:33:17 +0530

    firs commit

---
+++ src/main/java/com/edstem/jsonelasticsearch/JsonElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.edstem.jsonelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonElasticSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edstem/jsonelasticsearch/JsonElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstem.jsonelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
